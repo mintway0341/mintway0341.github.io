64556d568f867db387ec4191d01905d7204ea1ef
mintway0341 <44641348+mintway0341@users.noreply.github.com>
2019-07-12 01:29:00 -0400

    Add files via upload

---
+++ concodify.js
@@ -0,0 +1,53 @@
+function move_to_subscribe()
+{
+    window.location.href = "subscribe.html";
+}
+
+function change_color_trial_1()
+{
+    document.getElementById("button_trial").style.backgroundColor = '#0043A5';
+}
+function change_color_trial_2()
+{
+    document.getElementById("button_trial").style.backgroundColor = '#267EFF';
+}
+function change_color_month_1()
+{
+    document.getElementById("button_subscribe_month").style.backgroundColor = '#0043A5';
+}
+function change_color_month_2()
+{
+    document.getElementById("button_subscribe_month").style.backgroundColor = '#267EFF';
+}
+function change_color_year_1()
+{
+    document.getElementById("button_subscribe_year").style.backgroundColor = '#0043A5';
+}
+function change_color_year_2()
+{
+    document.getElementById("button_subscribe_year").style.backgroundColor = '#267EFF';
+}
+function change_color_mac_1()
+{
+    document.getElementById("download_btn_mac").style.backgroundColor = '#0043A5';
+}
+function change_color_mac_2()
+{
+    document.getElementById("download_btn_mac").style.backgroundColor = '#267EFF';
+}
+function change_color_windows_1()
+{
+    document.getElementById("download_btn_windows").style.backgroundColor = '#0043A5';
+}
+function change_color_windows_2()
+{
+    document.getElementById("download_btn_windows").style.backgroundColor = '#267EFF';
+}
+function change_color_linux_1()
+{
+    document.getElementById("download_btn_linux").style.backgroundColor = '#0043A5';
+}
+function change_color_linux_2()
+{
+    document.getElementById("download_btn_linux").style.backgroundColor = '#267EFF';
+}
